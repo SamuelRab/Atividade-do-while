e97062f6631f5cd7477285807382313df5b06c30
SamuelRab <samuelrabelodasilva@gmail.com>
2025-05-19 12:26:27 -0300

    index.js

--- atv 4/index.js
+++ atv 4/index.js
@@ -1,18 +1,18 @@
-let nome = "";
-let sobrenome = "";
+let nome = ""; // Variável que armazena o nome.
+let sobrenome = ""; // Variável que armazena o sobrenome.
 
 do{
-    nome = prompt("Digite o seu primeiro nome");
+    nome = prompt("Digite o seu primeiro nome"); // Pede para o usuário digitar seu primeiro nome.
 
     if (!isNaN(nome) || nome == "") {
-        alert("Você não digitou o seu nome")
+        alert("Você não digitou o seu nome") // Indica se o usuário não digitou seu nome.
     } else {
-       sobrenome = prompt("Digite o seu sobrenome agora");{
+       sobrenome = prompt("Digite o seu sobrenome agora");{ // Pede para o usuário digitar seu sobrenome.
         if (!isNaN(sobrenome || sobrenome == "" )) {
-            alert("Você não digitou o seu nome")
+            alert("Você não digitou o seu nome") // Indica se o usuário não digitou seu sobrenome.
        }
     }}
 
-}while (!isNaN(nome || nome == ""))
-    alert(nome)
-    alert(sobrenome)
+}while (!isNaN(nome || nome == "")) // Repetição do looping.
+    alert(nome) // Informa ao usuário seu nome.
+    alert(sobrenome) // Informa oa usuário o seu sobrenome.
